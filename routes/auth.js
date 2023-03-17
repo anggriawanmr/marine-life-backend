0be9af0fe1adb060e8e6d41e0c2cb91646d2d43f
anggriawanmr <anggriawanmr@gmail.com>
2023-03-17 16:03:44 +0700

    Editing auth route :hammer:

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,18 @@
 const router = require('express').Router();
 const passport = require('passport');
 
+router.get('/login/success', (req, res) => {
+  if (req.user) {
+    res.status(200).json({
+      error: false,
+      message: 'Successfully Logged In',
+      user: req.user,
+    });
+  } else {
+    res.status(403).json({ error: true, message: 'Not Authorized' });
+  }
+});
+
 router.get('/login/failed', (req, res) => {
   res.status(401).json({
     error: true,
@@ -15,3 +27,5 @@ router.get(
     failureRedirect: '/login/failed',
   })
 );
+
+router.get('/google', passport.authenticate('google', ['profile', 'email']));
