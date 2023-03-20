0feacac34a4cb9dbb731b5dbb80e80cfbcb9f594
anggriawanmr <anggriawanmr@gmail.com>
2023-03-20 16:44:59 +0700

    Finishing user controller :hammer:

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -29,3 +29,21 @@ const createUser = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+const getUserInfoByID = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const user = await User.findOne({ _id: id });
+
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({ message: 'User not found' });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+export { getAllUsers, createUser, getUserInfoByID };
