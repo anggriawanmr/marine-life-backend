2840ff48e94a6012bde088c9891d7b8030619a72
anggriawanmr <anggriawanmr@gmail.com>
2023-03-20 16:46:46 +0700

    Adding user routes :sparkles:

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,1 +1,16 @@
 import express from 'express';
+
+// Import all the controllers...
+import {
+  createUser,
+  getAllUsers,
+  getUserInfoByID,
+} from '../controllers/user.controller';
+
+const router = express.Router();
+
+router.route('/').get(getAllUsers);
+router.route('/').post(createUser);
+router.route('/:id').get(getUserInfoByID);
+
+export default router;
